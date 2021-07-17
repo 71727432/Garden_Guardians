ae227f1325aab29537505d7e1df4a6b13597dcac
71727432 <81695772+71727432@users.noreply.github.com>
2021-07-16 22:17:17 -0500

    a
    a

---
+++ Codigo_Garden_Guardian/Varibles_Globales/Global_Var.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+var vida_actual = 100
+var vida_Esqueleto = 60
+var Vida_Goblin = 80
+var Vida_Hongo = 80
+var Debilitar_esqueleto = false
+var Debilitar_Golem = false
+var Debilitar_Duende = false
+var Devilitar_Hongo = false
+var vida_Golem = 140
+
+
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
